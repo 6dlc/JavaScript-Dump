7097ddd6d1771fbb8cf69400c811c294f006c88c
6dlc <razielcadena@gmail.com>
2022-04-24 19:07:31 -0400

    Created onSubmit event

--- Index/indexMain.js
+++ Index/indexMain.js
@@ -1,3 +1,11 @@
 const userInfo = document.querySelector('#userInfo');
 const nameInput = document.querySelector('#name');
 const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.message');
+const userList = document.querySelector('#users');
+
+userInfo.addEventListener('submit', onsubmit);
+
+function onSubmit(e) {
+    
+}
