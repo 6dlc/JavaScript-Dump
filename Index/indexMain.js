6303cbe226d299c2179eb33cff6e933e5c2612dd
6dlc <razielcadena@gmail.com>
2022-04-24 19:31:22 -0400

    Added user list and created log for logins

--- Index/indexMain.js
+++ Index/indexMain.js
@@ -4,8 +4,24 @@ const emailInput = document.querySelector('#email');
 const msg = document.querySelector('.message');
 const userList = document.querySelector('#users');
 
-userInfo.addEventListener('submit', onsubmit);
+userInfo.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
-    
+    e.preventDefault();
+
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all required fields.';
+
+        setTimeout(() => msg.remove(), 5000)
+    } else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
 }
